6096cf24c18aa26bd80e38e749e2d856008efe48
Vishvajit Kher <vishvajit79@gmail.com>
2017-11-07 22:03:39 -0500

    activities added

--- app/src/main/java/com/example/vishv/a2101015270/MainActivity.java
+++ app/src/main/java/com/example/vishv/a2101015270/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.vishv.a2101015270;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.QuickContactBadge;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
@@ -26,5 +30,24 @@ public class MainActivity extends AppCompatActivity {
             Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
+
+        Button loginBtn = findViewById(R.id.main_login_btn);
+        Button registerBtn = findViewById(R.id.main_register_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
